701fb9b2f75d9ca4e518ff5e2c991a9dae3db977
Sushant Adhikari <34622983+adhikarisushant@users.noreply.github.com>
2023-11-10 17:31:50 +0545

    dil up

--- src/pages/products.js
+++ src/pages/products.js
@@ -1,22 +1,63 @@
-import React from "react";
-import "react-responsive-carousel/lib/styles/carousel.min.css";
-import { Carousel } from "react-responsive-carousel";
-import Slider from "react-slick";
-
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Link, useParams } from "react-router-dom";
+import logo from "../assets/img/logo.png";
+const API_URI = "https://admin.massholdings.com.np/api/products/10/1";
 const Products = () => {
+  const { slug } = useParams();
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  useEffect(() => {
+    getApiData();
+  }, [slug]);
+  async function getApiData() {
+    try {
+      const response = await axios.get(API_URI);
+      setData(response.data);
+      console.log("P-------------------", response);
+    } catch (error) {
+      setError(error);
+    }
+    setLoading(false);
+  }
+
+  if (loading) return "...";
+  if (error) return "error";
   return (
     <>
       <div className="Breadcom">
-        <div></div>
+        <div className="container">
+          <h1>{slug}</h1>
+          <ul>
+            <li>Home</li>
+            <li>Products</li>
+            <li>{slug}</li>
+          </ul>
+        </div>
       </div>
       <div className="ProductsDetails">
         <div className="container">
           <div className="row">
-            <div className="col-sm-4 col-md-3 col-lg-2">
-              <div className="FilterNav">Filter</div>
-            </div>
-            <div className="col-sm-8 col-md-9 col-lg-10">
-              <div>Products</div>
+            {/* <div className="col-sm-5 col-md-4 col-lg-3">
+              <div className="FilterNav">
+                <h3>Filter</h3>
+              </div>
+            </div> */}
+            <div className="col-sm-12 col-md-12 col-lg-12">
+              {data.items.map((i) => {
+                console.log("it", i.slug);
+                return (
+                  <div className="ProductsItems">
+                    <div className="productsImage">
+                      <img src={i.DocPath} />
+                    </div>
+                    <div className="productsTitle">
+                      <h1>{i.item_name}</h1>
+                    </div>
+                  </div>
+                );
+              })}
             </div>
           </div>
         </div>
